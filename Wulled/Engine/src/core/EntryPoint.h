2704c5b5e75603b94c20fd34f1b1077ec22d63cd
Forward-Fax3 <dfordommmmm@gmail.com>
2022-11-27 11:34:37 +0000

    just a save

--- Wulled/Engine/src/core/EntryPoint.h
+++ Wulled/Engine/src/core/EntryPoint.h
@@ -10,19 +10,11 @@ extern WLD::Application* WLD::CreateApplication(bool* run);
 int main()
 {
 	WLD::Log::Init();
-<<<<<<< HEAD
 	bool run[2] = { true, false };
 	std::thread command = std::thread(Command, run);
 	command.detach();
 
-	do
-=======
-	WLD_CORE_WARNING("initialied log");
-	LOG_INFO("hello world");
-	bool run[2];
-
 	do 
->>>>>>> 80383ae7499a6d4517d3792745d7aebde7548779
 	{
 		run[0] = true;
 		run[1] = false;
